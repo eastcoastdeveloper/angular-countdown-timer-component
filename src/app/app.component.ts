5f60f0d94b0dbcd172949c4ea31613c79474fc67
eastcoastdeveloper <58962475+eastcoastdeveloper@users.noreply.github.com>
2024-07-19 21:41:35 -0700

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 export class AppComponent implements AfterViewInit {
   date: any;
   now: any;
-  targetDate: any = new Date(2024, 5, 11);
+  targetDate: any = new Date(2025, 5, 11);
   targetTime: any = this.targetDate.getTime();
   difference: number;
   months: Array<string> = [
